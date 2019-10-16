3b05b9ed304029bbcc44a232e6dbf35d7f27cf00
Wallace <wbalaniu@my.centennialcollege.ca>
2019-10-16 14:06:15 -0400

    Added lasers shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Public variables
     public float speed;
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float fireRate = 0.5f;
+
+    // Private variables
+    private Rigidbody2D rBody;
+    private float counter = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && counter > fireRate)
+        {
+            // Create and position my laser object
+            Instantiate(laser, laserSpawn.position, laser.transform.rotation);
+            counter = 0.0f;
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +42,7 @@ public class PlayerController : MonoBehaviour
         // Debug.Log("Horizontal: " + horiz + " Vertical: " + vert);
 
         // Modify players velocity through the rigidbody2D
-        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        
         rBody.velocity = new Vector2(horiz * speed, vert * speed);
     }
 }
