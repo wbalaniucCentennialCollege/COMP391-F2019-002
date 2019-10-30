db4c6f3691a4e4cd60fcaa8da171e3f0e9960b85
Wallace <wbalaniu@my.centennialcollege.ca>
2019-10-30 13:28:08 -0400

    Added Boundary and Boundary Destroyer

---
+++ Assets/Scripts/BoundaryDestoryer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDestoryer : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject); // Destroy the entire game object
+    }
+}
