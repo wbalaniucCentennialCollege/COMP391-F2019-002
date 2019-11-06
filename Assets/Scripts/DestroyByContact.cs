7df18af88666512c96d44d4922c00bbb6003cd0f
Wallace <wbalaniu@my.centennialcollege.ca>
2019-11-06 13:13:25 -0500

    Added exploding asteroids

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Instantiate our explosion prefab at the correct location
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        // Destroy the "other" object and destroy this asteroid
+        Destroy(other.gameObject); // Laser
+        Destroy(this.gameObject);
+    }
+}
